e4c9519c99f031e992ae39e2f0a0d891d25952c2
Usernow <usernow.82@gmail.com>
2022-02-27 12:30:59 -0500

    fix

--- src/main/java/com/spring/universidad/ubackend/repositorios/ProfesorRepository.java
+++ src/main/java/com/spring/universidad/ubackend/repositorios/ProfesorRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
-//@Repository("repositorioProfesores")
+@Repository("repositorioProfesores")
 public interface ProfesorRepository extends PersonaRepository{
 
     @Query("select p from Profesor as p join fetch p.carreras as c where c.nombre =?1")
